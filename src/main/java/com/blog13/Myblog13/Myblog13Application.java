29a93b37ef1f814c10c5223107a478b6345e810f
Koushik <koushikdey1244@gmail.com>
2024-01-14 20:04:21 +0530

    127:Create Save Post Feature

---
+++ src/main/java/com/blog13/Myblog13/Myblog13Application.java
@@ -0,0 +1,13 @@
+package com.blog13.Myblog13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog13/Myblog13/Myblog13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog13.Myblog13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
